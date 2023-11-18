f8c34e0c0a3278d8fdf3efb74a6ad3faff973f4c
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2023-11-18 13:45:34 -0500

    Adding prometheus, collector and metrics

--- dash_otel/app.py
+++ dash_otel/app.py
@@ -1,13 +1,22 @@
+import logging
+
 from opentelemetry import trace
-
-tracer = trace.get_tracer('dash')
-
-
+from opentelemetry import metrics
 from dash import Dash, html, dcc, callback, Output, Input, ctx
 import plotly.express as px
 import pandas as pd
 import requests
 
+tracer = trace.get_tracer('dash')
+
+meter = metrics.get_meter("dash-otel.meter")
+
+num_callback = meter.create_counter(
+    "dash-otel.num-callback",
+    description="total number of callbacks ran"
+)
+
+logger = logging.getLogger(__name__)
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
@@ -25,6 +34,8 @@ app.layout = html.Div([
 )
 @tracer.start_as_current_span("update-graph.callback")
 def update_graph(value):
+    logger.error("testing out otel logging")
+    num_callback.add(1)
     with tracer.start_as_current_span("update-graph") as span: 
         span.set_attribute("dash.context", ctx.triggered_id or "none")   
         response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
